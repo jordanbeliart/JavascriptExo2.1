8d596dcafe99273709b6f136a01c7f98ec1663af
jordanbeliart <j.beliart.jb@gmail.com>
2019-11-29 09:41:55 +0100

    ajout nom fonction

--- assets/js/style.js
+++ assets/js/style.js
@@ -4,9 +4,11 @@ ou bien remplacer 'getElementById()' par 'querySelector('img')' */
 var image = document.getElementById('photo');
 /* 'mouseover' --> action déclenchée au survol de la souris
 'mouseout' --> action déclenchée dès que la souris ne survol plus l'élément concerné */
-image.addEventListener('mouseover', function(){
+image.addEventListener('mouseover', image2)
+function image2(){
   image.src ='assets/img/img2.jpg';
-});
-image.addEventListener('mouseout', function(){
+};
+image.addEventListener('mouseout', image1)
+function image1(){
   image.src ='assets/img/img1.jpeg';
-});
+};
